1f3616a44ed4438d6cf363163d3045fcf4214e57
gsansone4 <gsansone4@gmail.com>
2018-08-09 18:33:14 -0400

    values adjusted, color mapping

--- sketch.js
+++ sketch.js
@@ -1,17 +1,17 @@
 var cols, rows;
 var scl = 20;
-var w = 1400;
-var h = 1000;
+var w = window.screen.availWidth*1.75;
+var h = window.screen.availHeight;
 
 var flying = 0;
 
 var terrain = [];
 
 function setup() {
-  createCanvas(600, 600, WEBGL);
+  createCanvas(windowWidth, windowHeight, WEBGL);
   cols = w / scl;
   rows = h/ scl;
-
+  frameRate(30);
   for (var x = 0; x < cols; x++) {
     terrain[x] = [];
     for (var y = 0; y < rows; y++) {
@@ -34,14 +34,16 @@ function draw() {
   }
 
 
-  background(0);
+  background(255);
   translate(0, 50);
   rotateX(PI/3);
-  fill(200,200,200, 25);
+  //fill(200,200,200, 50);
   translate(-w/2, -h/2);
+  noStroke();
   for (var y = 0; y < rows-1; y++) {
     beginShape(TRIANGLE_STRIP);
     for (var x = 0; x < cols; x++) {
+      fill (map(terrain[x][y], -100, 100, 0, 255, true), map(terrain[x][y], -100, 100, 255, 0, true), 0);
       vertex(x*scl, y*scl, terrain[x][y]);
       vertex(x*scl, (y+1)*scl, terrain[x][y+1]);
     }
